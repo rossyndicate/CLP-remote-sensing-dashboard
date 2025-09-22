5afbbfc19e89f3ab862e797d46f796a96cad4852
Juan De La Torre <141076961+juandlt-csu@users.noreply.github.com>
2025-09-22 10:41:36 -0600

    Update R/load_sdd_data.R
    Co-authored-by: B Steele <32140074+steeleb@users.noreply.github.com>

--- R/load_sdd_data.R
+++ R/load_sdd_data.R
@@ -4,7 +4,7 @@ load_temp_data <- function(sdd_data_path) {
   tryCatch({
     sdd <- arrow::read_feather(sdd_data_path, as_data_frame = TRUE)
   }, error = function(e) {
-    showNotification("Error loading reservoir data", type = "error")
+    showNotification("Error loading waterbody data", type = "error")
   })
   
   return(sdd)
